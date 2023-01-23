5a09dd2fd088e88daa0dc9dc15af432d11126456
silva.thiago_ifood <silva.thiago_ifood@13796-NB>
2023-01-23 19:26:33 -0300

    Adicionar arquivos de projeto.

---
+++ AppControSmart/AppControSmart/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AppControSmart.ViewModels;
+using AppControSmart.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AppControSmart
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ AppControSmart/AppControSmart/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AppControSmart.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppControSmart.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
